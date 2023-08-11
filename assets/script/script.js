41e4c57d564da556ce2e22bfd1b2c0119eb4d823
Marine Porret <marine.porret@gmail.com>
2023-08-11 15:55:51 +0200

    First commit

---
+++ assets/script/script.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // Ouvrir et fermer la modal
+  document.querySelector("#contact").addEventListener("click", (e) => {
+    document.querySelector("#modal").classList.remove("hidden");
+  });
+
+  document.querySelector("form button").addEventListener("click", async (e) => {
+    e.preventDefault();
+
+    const data = {
+      firstname: document.querySelector("#firstname").value,
+      lastname: document.querySelector("#lastname").value,
+      email: document.querySelector("#email").value,
+      message: document.querySelector("#message").value,
+    };
+    console.log(data);
+
+    const response = await axios.post(
+      "https://site--tripadvisor-server--hxhcg25qdky2.code.run/form",
+      data
+    );
+
+    console.log(response);
+    document.querySelector("#modal").classList.add("hidden");
+  });
+});
